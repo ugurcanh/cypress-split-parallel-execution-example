9a1d39a5295bdebee6b992945a0eba7a67117b0e
Ugurcan Hosafci <ugurcanhosafci@gmail.com>
2023-03-12 22:25:45 +0300

    first test is resolved

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const cypressSplit = require("cypress-split");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("file:preprocessor", cucumber());
+      cypressSplit(on, config);
+
+      return config;
     },
 
     specPattern: "cypress/integration/**/*.feature",
--- cypress/integration/FeatureBasedTests/parallelExecutionGoogle/firstParallelTest.js
+++ cypress/integration/FeatureBasedTests/parallelExecutionGoogle/firstParallelTest.js
@@ -1,9 +1,12 @@
 import {When, Then} from "cypress-cucumber-preprocessor/steps";
 
 When("Navigate to Google page", ()=>{
+    //Visit Google page 
     cy.visit("www.google.com");
 });
 
 Then("Assert Google Search button is loaded", () => {
+    //Assert there is a button (Actually its text :) )
+    cy.contains("Gmail").should("be.visible");
 
 });
